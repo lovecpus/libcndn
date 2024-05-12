8e3245b3a8ad2c805c50a6884dc6526712e64d4d
XPERON <golftongapp@gmail.com>
2024-05-12 14:37:25 +0900

    add crc16_ccitt

--- src/libcndn.hpp
+++ src/libcndn.hpp
@@ -296,6 +296,21 @@ public:
 			}
 		}
 	}
+
+	unsigned short CRC16(uint16_t& crc, const uint8_t * crcdata, size_t len) {
+	  for(size_t i=0;i<len;i++){
+	    crc = crc ^ ((*crcdata)<<8);
+	    crcdata++;                 
+	    for(uint8_t j=0;j<8;j++){
+	      if(crc & 0x8000){
+	        crc = (crc<<1) ^ 0x1021;              
+	      }else{
+	        crc = crc<<1;    
+	      }           
+	    }
+	  }
+	  return crc;                        
+	}	
 };
 
 class CNStream : public Stream
