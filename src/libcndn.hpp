bf57a3e28a6e7c4f911817caa2c9e49c592f422f
TAEGYOON KIM <lovecpus@naver.com>
2024-05-19 16:20:01 +0900

    Add Arduino headers

--- src/libcndn.hpp
+++ src/libcndn.hpp
@@ -29,6 +29,38 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define STRDEFINE(str) VALDEFINE(str)
 
 #include <Arduino.h>
+#if ARDUINO > 100
+#include <Wire.h>
+#endif
+
+#ifndef lowByte
+#define lowByte(w) ((uint8_t) ((w) & 0xff))
+#define highByte(w) ((uint8_t) ((w) >> 8))
+#endif
+
+#ifndef bitRead
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#endif
+
+#ifndef bitRead
+#define bitRead(value, bit) ((value) |= (1UL << (bit)))
+#endif
+
+#ifndef bitClear
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#endif
+
+#ifndef bitToggle
+#define bitToggle(value, bit) ((value) ^= (1UL << (bit)))
+#endif
+
+#ifndef bitWrite
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#endif
+
+#ifndef bit
+#define bit(b) (1UL << (b))
+#endif
 
 class CNTimeout
 {
